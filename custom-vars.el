702be255d26fcbcad3de2e32de15baaa0ecd6b29
Aagaman Luitel <mail@luitelaagaman.com.np>
2023-02-21 17:29:24 +0545

    add custom vars

---
+++ custom-vars.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eglot-ignored-server-capabilities '(:hoverProvider))
+ '(package-selected-packages
+   '(dashboard markdown-mode projectile hl-todo savehist use-package devdocs solaire-mode solarie-mode multi-vterm org-modern corfu marginalia lsp-mode geiser-mit web-mode vterm flycheck-rust tuareg merlin editorconfig nyan-mode all-the-icons doom-modeline elfeed evil-collection which-key general aggressive-indent magit consult vertico flycheck orderless tree-sitter-langs rust-mode go-mode smartparens lsp-ui corfu-prescient doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
